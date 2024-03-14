f961c916a12266f68b549c171ee785afa78e094a
Alvin Kimata <alvinkimata@gmail.com>
2024-03-15 00:04:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -222,7 +222,7 @@ if _is_cuda() and not compute_capabilities:
     device_count = torch.cuda.device_count()
     for i in range(device_count):
         major, minor = torch.cuda.get_device_capability(i)
-        if major < 7:
+        if major < 6:
             raise RuntimeError(
                 "GPUs with compute capability below 7.0 are not supported.")
         compute_capabilities.add(f"{major}.{minor}")
